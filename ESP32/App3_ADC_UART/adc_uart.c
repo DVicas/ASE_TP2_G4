a9baa74962231ac04725798a27566e74dd96acf7
79AFonso <apteixeira@ua.pt>
2022-04-14 16:58:02 +0100

    uart 1st task implement

--- ESP32/App3_ADC_UART/adc_uart.c
+++ ESP32/App3_ADC_UART/adc_uart.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
+#include "driver/uart.h"
+#include "sdkconfig.h"
+#include "esp_rom_gpio.h"
+#include "soc/uart_periph.h"
+
+#include "esp_timer.h"
+#include "esp_sleep.h"
+#include "esp_log.h"
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES   64          //Multisampling
@@ -20,10 +30,52 @@ static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
 static const adc_atten_t atten = ADC_ATTEN_DB_0;
 static const adc_unit_t unit = ADC_UNIT_1;
 
+static const int BUF_SIZE = 1024;
+#define DEFAULT_UART_RX_PIN     (4)
+#define DEFAULT_UART_TX_PIN     (5)
 
+uint32_t adc_reading = 0;
+
+void init(void) {
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_0, &uart_config);
+    uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    esp_rom_gpio_connect_out_signal(DEFAULT_UART_RX_PIN, UART_PERIPH_SIGNAL(1, SOC_UART_TX_PIN_IDX), false, false);
+    esp_rom_gpio_connect_in_signal(DEFAULT_UART_RX_PIN, UART_PERIPH_SIGNAL(0, SOC_UART_RX_PIN_IDX), false);
+
+    esp_rom_gpio_connect_out_signal(DEFAULT_UART_TX_PIN, UART_PERIPH_SIGNAL(0, SOC_UART_TX_PIN_IDX), false, false);
+    esp_rom_gpio_connect_in_signal(DEFAULT_UART_TX_PIN, UART_PERIPH_SIGNAL(1, SOC_UART_RX_PIN_IDX), false);
+}
+
+static void uart_read_task(void *arg){
+    while (1) {
+        
+        const int len = sizeof(adc_reading);
+        const int txBytes = uart_write_bytes(UART_NUM_1, &adc_reading, len);
+        printf("UART0 sending to UART1: %d\n", adc_reading);
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
 
 void app_main(void)
 {
+
+    init();
 
     //Configure ADC
     adc1_config_width(width);
@@ -35,7 +87,6 @@ void app_main(void)
 
     //Continuously sample ADC1
     while (1) {
-        uint32_t adc_reading = 0;
 
         adc_reading = adc1_get_raw((adc1_channel_t)channel);
         
@@ -43,5 +94,7 @@ void app_main(void)
         uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
         printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
         vTaskDelay(pdMS_TO_TICKS(1000));
+
+        xTaskCreate(uart_read_task, "uart_read_task", 2048, NULL, 10, NULL);
     }
 }
