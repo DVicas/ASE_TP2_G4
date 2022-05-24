0f935eedb49009feb558391609709cfcd14067dd
Diogo Vicente <45186218+DVicas@users.noreply.github.com>
2022-05-24 11:37:58 +0100

    Update temp.py

--- App_RPi/temp.py
+++ App_RPi/temp.py
@@ -7,16 +7,6 @@ temp = 0x00
 
 bus = smbus.SMBus(i2c_ch)
 
-#while True:
-
-#    raw_val = bus.read_i2c_block_data(i2c_addr, temp, 2)
-#    final_temp = (raw_val[0] << 4) | (raw_val[1] >> 4)
-#    final_temp = final_temp*0.0625
-
-#    print(final_temp)
-
-#    time.sleep(2)
-
 f = open("temps.txt", "w")
 
 for i in range(0,1):
@@ -26,6 +16,3 @@ for i in range(0,1):
     final_temp = final_temp*0.0625
     
     f.write(f"{final_temp} ")
-    
-
-    
